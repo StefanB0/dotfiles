caaa3bbe74163809e1c323a5f360cf186b2c4746
Stefan Boicu <stefan.boicu@ebs-integrator.com>
2025-05-19 18:01:52 +0300

    update install script

--- dot_config/install/windows-wsl.ps1
+++ dot_config/install/windows-wsl.ps1
@@ -4,17 +4,19 @@
 Write-Host "Installing Windows Subsystem for Linux..." -ForegroundColor Cyan
 
 wsl --install Debian
-
-### Wait for initial Debian setup (user must complete setup manually)
-Write-Host "Waiting for Debian to initialize. Complete user setup, then press Enter to continue..."
-Pause
-
 wsl --set-default Debian
 
+Write-Host "Waiting for Debian to initialize. Complete the user setup (IN ANOTHER TAB), then press Enter to continue..."
+Pause
+
 ### Run the install script inside WSL
-$WSLTOHOME = "/mnt/c/Users/$env:USERNAME/"
+Write-Host "Configuring WSL distribution..." -ForegroundColor Cyan
+
+$WSLTOHOME = "/mnt/c/Users/$env:USERNAME"
 
 wsl -d Debian bash -c "mkdir -p ~/.config/install ~/.config/keys/"
 wsl -d Debian bash -c "cp $WSLTOHOME/.config/keys/chezmoi-key.txt ~/.config/keys/chezmoi-key.txt"
 wsl -d Debian bash -c "cp $WSLTOHOME/.config/install/debian.sh ~/.config/install/debian.sh"
 wsl -d Debian bash -c "chmod +x ~/.config/install/debian.sh && ~/.config/install/debian.sh"
+
+Pause
